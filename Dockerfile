852093c417e91eaab174d5db40a20c7f34a77df2
Ivan Barabanshikov <barabanzz871@gmail.com>
2020-12-19 21:55:20 +0300

    edit dockerfile v15

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,11 @@ FROM node:12.17.0-alpine
 WORKDIR /app
 COPY . .
 RUN npm install --silent
+RUN cd ./client && npm install --silent
+RUN cd ..
 RUN npm install react-scripts typescript -g --silent 
-RUN npm run client:build
+RUN cd ./client
+RUN npm run build
+RUN cd ..
 
 ENTRYPOINT ["node", "app.js"]
--- db/connect.ts
+++ db/connect.ts
@@ -4,10 +4,13 @@ import pgPromise from 'pg-promise';
 const dbPort = keys.dbPort;
 const dbName = keys.dbName;
 const dbPassword = keys.dbPassword;
-// const dbHost = keys.dbHost;
+const dbHost = process.env.IS_DOCKER? 'postgres' : keys.dbHost;
 const dbDatabase = keys.dbDatabse;
 
+console.log(process.env.dbTest);
+
+
 const pgp = pgPromise();
-const db = pgp(`postgres://${dbName}:${dbPassword}@${"postgres"}:${dbPort}/${dbDatabase}`);
+const db = pgp(`postgres://${dbName}:${dbPassword}@${dbHost}:${dbPort}/${dbDatabase}`);
 
 export default db;
